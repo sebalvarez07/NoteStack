b7c136cf9256a7c59d42b63ebdd06361dfee604b
Sebastian Alvarez <>
2019-04-30 19:23:33 -0400

    Fixing issue with firebase fatal error

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 if(process.env.NODE_ENV === 'test'){
     // Brings in the variables from .env.test so they're accessible to us here
     require('dotenv').config({ path: '.env.test'});
-} else if(process.env.NODE_ENV === 'development'){
+} else {
     // Brings in the variables from .env.development so they're accessible to us here
     require('dotenv').config({ path: '.env.development'});
 }
