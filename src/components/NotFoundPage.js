892ac3bf9da8d6b8e11f7f1162aed194958b80e8
Sebastian Alvarez <>
2019-06-06 17:22:07 -0400

    condition auth for 404

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -13,34 +13,50 @@ const NotFoundPage = (props) => {
 
     return (
         <div>
-            <div 
-                id='wrapper'
-                className={`wrapper ${ props.sidebarCollapsed ? 'sidebar-collapse' : '' } ${ props.headerCollapsed ? 'header-collapse' : '' }` }>
-                <Sidebar />
-                { !props.sidebarCollapsed && <div className='sidebar-overlay--mobile'></div> }
-                <div className='content-page'>
-                    <div className='header-wrapper'>
-                        <Header />
-                    </div>
+            { props.isAuthenticated ?
+                <div
+                    id='wrapper'
+                    className={`wrapper ${ props.sidebarCollapsed ? 'sidebar-collapse' : '' } ${ props.headerCollapsed ? 'header-collapse' : '' }` }>
+                    <Sidebar />
+                    { !props.sidebarCollapsed && <div className='sidebar-overlay--mobile'></div> }
+                    <div className='content-page'>
+                        <div className='header-wrapper'>
+                            <Header />
+                        </div>
 
-                    <div className='dashboard-container'>
-                        <div className='content-container'>
-                            <WidgetMessage 
-                                title={'Whoops!'}
-                                msg={'Can’t find what you’re looking for...'}
-                                imgPath={`/images/404-icon.png`}
-                                widgetAction={returnToDashboard}
-                                actionButtonTxt={'Return to Dashboard'}
-                            />
+                        <div className='dashboard-container'>
+                            <div className='content-container'>
+                                <WidgetMessage 
+                                    title={'Whoops!'}
+                                    msg={'Can’t find what you’re looking for...'}
+                                    imgPath={`/images/404-icon.png`}
+                                    widgetAction={returnToDashboard}
+                                    actionButtonTxt={'Return to Dashboard'}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                :
+                <div className='dashboard-container'>
+                    <div className='content-container'>
+                        <WidgetMessage 
+                            title={'Whoops!'}
+                            msg={'Can’t find what you’re looking for...'}
+                            imgPath={`/images/404-icon.png`}
+                            widgetAction={returnToDashboard}
+                            actionButtonTxt={'Return to Dashboard'}
+                        />
+                    </div>
+                </div>
+            }
+            
         </div>
     )
 };
 
 const mapStateToProps = (state) => ({
+    isAuthenticated: !!state.auth.uid,
     headerCollapsed: state.collapseStatus.headerCollapsed,
     sidebarCollapsed: state.collapseStatus.sidebarCollapsed,
 });
